453bc9899e70461eb780cea625b15ee880afe778
zhouxingprojet <axelzhou.3@gmail.com>
2026-08-27 21:48:25 +0200

    Publish PHYEL graphic design portfolio

--- menu.js
+++ menu.js
@@ -7,6 +7,7 @@
     ['index.html', 'Accueil'],
     ['works.html', 'Œuvres'],
     ['portfolio.html', 'Projets'],
+    ['graphic-design.html', 'Design graphique'],
     ['x-art-lab.html', 'X-ART Lab'],
     ['Cv.html', 'Biographie / CV'],
     ['Contact.html', 'Contact'],
